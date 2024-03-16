bb362cbd592d59e9075d3240f4cf8d0aae35e650
Mar1aP <platonovamaria2004@gmail.com>
2024-03-16 20:00:38 +0500

    New test and function fix

--- TRPO-Lab3.Lib/SphereLayer.cs
+++ TRPO-Lab3.Lib/SphereLayer.cs
@@ -4,7 +4,10 @@
     {
         public static double FindArea(double radius1, double radius2, double height)
         {
-            return Math.PI / 6 * height * (3*radius1*radius1 + 3*radius2*radius2 + height*height);
+            if (radius1 < 0 || radius2 < 0 || height < 0)
+                throw new ArgumentException("Входные данные не должны быть отрицательными.");
+
+            return Math.PI / 6 * height * (3 * radius1 * radius1 + 3 * radius2 * radius2 + height * height);
         }
     }
 }
--- TRPO-Lab3.Tests/UnitTest1.cs
+++ TRPO-Lab3.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using TRPO_Lab3.Lib;
+
 namespace TRPO_Lab3.Tests
 {
     public class Tests
@@ -10,7 +12,33 @@ namespace TRPO_Lab3.Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            double actual = SphereLayer.FindArea(15, 30, 5);
+            double expected = 8901.179;
+            Assert.Greater(0.001, Math.Abs(actual - expected));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            double actual = SphereLayer.FindArea(5, 10, 5);
+            double expected = 1047.198;
+            Assert.Greater(0.001, Math.Abs(actual - expected));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            double actual = SphereLayer.FindArea(20, 10, 10);
+            double expected = 8377.580;
+            Assert.Greater(0.001, Math.Abs(actual - expected));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.Throws(typeof(ArgumentException), () => { SphereLayer.FindArea(-20, 10, 10); });
+            Assert.Throws(typeof(ArgumentException), () => { SphereLayer.FindArea(20, -10, 10); });
+            Assert.Throws(typeof(ArgumentException), () => { SphereLayer.FindArea(20, 10, -10); });
         }
     }
 }
