5116e980db71804d921c8c67d711f5fa1ffefe40
Mar1aP <platonovamaria2004@gmail.com>
2024-03-16 19:59:02 +0500

    New formula

---
+++ TRPO-Lab3.Lib/SphereLayer.cs
@@ -0,0 +1,10 @@
+﻿namespace TRPO_Lab3.Lib
+{
+    public class SphereLayer
+    {
+        public static double FindArea(double radius1, double radius2, double height)
+        {
+            return Math.PI / 6 * height * (3*radius1*radius1 + 3*radius2*radius2 + height*height);
+        }
+    }
+}
