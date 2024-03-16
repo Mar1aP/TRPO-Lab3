f19146d4a30df872501211169c93df55e3c7a98a
Mar1aP <platonovamaria2004@gmail.com>
2024-03-16 20:26:09 +0500

    Console app

--- TRPO-Lab3.ConsoleApp/Program.cs
+++ TRPO-Lab3.ConsoleApp/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace TRPO_Lab3.ConsoleApp
+﻿using TRPO_Lab3.Lib;
+
+namespace TRPO_Lab3.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Программа по нахождению объёма шарового слоя");
+
+            Console.WriteLine("Первый радиус: ");
+            double radius1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Второй радиус: ");
+            double radius2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Высота слоя: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine("Объём шарового слоя: " + SphereLayer.FindVolume(radius1, radius2, height));
+            }
+            catch(ArgumentException exc)
+            {
+                Console.WriteLine("Ошибка при расчёте: " + exc.Message);
+            }
         }
     }
 }
