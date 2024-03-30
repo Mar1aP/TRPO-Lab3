598bc09d1ea730a11eb5a531e504775f136755ea
Mar1aP <platonovamaria2004@gmail.com>
2024-03-30 20:11:24 +0500

    View model

---
+++ TRPO-Lab3.AspNetApp/Models/SphereLayerViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TRPO_Lab3.AspNetApp.Models
+{
+    public class SphereLayerViewModel
+    {
+        public double Radius1 { get; set; }
+        public double Radius2 { get; set; }
+        public double Height { get; set; }
+        public string Volume { get; set; }
+    }
+}
